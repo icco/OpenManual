33199c1a28eaac00508640d499e3be29ea850758
Nat Welch <nat@natwelch.com>
2010-10-06 12:21:55 -0700

    Fix pygments.

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 # Given an XML file, turn into html and insert it into standard.html
 # Uses Pygments to color xml file : http://pygments.org/docs/cmdline/
 
-PYG='/opt/local/bin/pygmentize-2.6'
+PYG=`which pygmentize`
 
 $PYG -f html -l xml $1
 
